719674ef0a81366f2b8ed212b414e6457cbf2f94
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2019-06-14 14:58:46 -0700

    Create game title to main menu

---
+++ SwiftSnake/GameManager.swift
@@ -0,0 +1,9 @@
+//
+//  GameManager.swift
+//  SwiftSnake
+//
+//  Created by Injoong Yoon on 6/14/19.
+//  Copyright © 2019 Injoong Yoon. All rights reserved.
+//
+
+import Foundation
--- SwiftSnake/GameScene.swift
+++ SwiftSnake/GameScene.swift
@@ -11,12 +11,29 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var gameLogo: SKLabelNode!
+    var bestScore: SKLabelNode!
+    var playButton: SKShapeNode!
+    
     override func didMove(to view: SKView) {
-        
+        initializeMenu()
     }
-        
+    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+    }
+    
+    private func initializeMenu() {
+        // Create game title
+        gameLogo = SKLabelNode(fontNamed: "ArialRoundedMTBold")
+        gameLogo.zPosition = 1
+        gameLogo.position = CGPoint(x: 0, y: (frame.size.height / 2) - 200)
+        gameLogo.fontSize = 60
+        gameLogo.text = "SNAKE"
+        gameLogo.fontColor = SKColor.red
+        self.addChild(gameLogo)
+        
+        // Create best score label
     }
 }
